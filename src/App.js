b7b43795c57f3ab5daebf2341c59d2f63702765a
OsamaQureshi147 <osamahsan@gmail.com>
2021-01-20 19:02:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Osama Ehsan</h1>
+      <h1>Hello from Osama Ehsan after some changes</h1>
     </div>
   );
 }
